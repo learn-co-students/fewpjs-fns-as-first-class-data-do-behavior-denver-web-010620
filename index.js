1fb43796087de7ee8a8b70132899dffc68ff50e5
Zprogrammer-code <skatboyza124@hotmail.com>
2020-02-24 13:18:44 -0700

    Done.

--- index.js
+++ index.js
@@ -5,7 +5,26 @@ function handleClick(e) {
   displayMessage(greet(timeString))
 }
 
+function greet(time1){
+  if(time1 > 1:00 && time1 < 12:00){
+    return "Good Morning";
+  } else if(time1 > 12:00 && time1 < 5:00) {
+    return "Good AfterNoon"
+  } else {
+    return "Good Evening"
+  }
+}
+
 /* End Given Code, don't edit above here...*/
+function displayMessage(greeting){
+  const greetings = document.querySelector('#greeting')
+ const theGreeting = document.createElement('div')
+ theGreeting.innerText = greet()
+ greetings.appendChild(theGreeting)
+
+
+
+}
 
 /* Write your implementation of greet() */
 /* Write your implementation of displayMessage() */
